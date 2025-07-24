4528bc7b47ad15cced4c773e151b1153dcdc13cf
OkuJoe <joeokurut776@gmail.com>
2025-07-24 05:42:53 -0700

    Add files via upload

---
+++ madlib game.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int main (){
+string color;
+string pluralnoun;
+string celebrity;
+
+cout<<"enter colour:"<<endl;
+getline(cin,color);
+cout<<"enter pluralnoun:"<<endl;
+getline(cin,pluralnoun);
+cout<<"enter celeb name:"<<endl;
+getline(cin,celebrity);
+
+cout<< "Roses are:"<< color <<endl;
+cout <<pluralnoun << "i am Him:"<<endl;
+cout<< "i love:"<< celebrity; 
+
+return 0;
+}
